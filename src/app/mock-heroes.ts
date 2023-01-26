63d00dabd6e38b8efb677030afd83d982129102e
GiselleBarbosa <g88barbosa@gmail.com>
2023-01-26 16:48:56 -0300

    update list heroes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 1, name: 'Luffy' },
+  { id: 2, name: 'Roronoa Zoro' },
+  { id: 3, name: 'Nami' },
+  { id: 4, name: 'Usopp' },
+  { id: 5, name: 'Sanji' },
+  { id: 6, name: 'Chopper' },
+  { id: 7, name: 'Nico Robin' },
+  { id: 8, name: 'Franky' },
+  { id: 9, name: 'Brook' },
+  { id: 10, name: 'Jimbe' },
 ];
